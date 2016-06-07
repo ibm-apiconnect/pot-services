2184e169c7afac3176cf6088fe7078c12b31d5fa
Tim Baker <bakert@us.ibm.com>
2016-06-07 12:37:50 -0500

    fix to add HEAD listener

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/', function(req, res) {
+router.all('/', function(req, res) {
   res.sendStatus(200);
 });
 
--- routes/financing.js
+++ routes/financing.js
@@ -3,42 +3,46 @@ var router = express.Router();
 var xml2js = require('xml2js');
 
 /* Calculate Financing Rate from SOAP input */
-router.post('/calculate', function (req, res) {
-  console.log(req.body);
-
-  var parseString = require('xml2js').parseString;
-  var parseOptions = {
-    trim: true,
-    explicitArray: false,
-    ignoreAttrs: true,
-    explicitRoot: false
-  };
-
-  parseString(req.body, parseOptions, function (err, result) {
-    console.log("XML as JSON:\n" + JSON.stringify(result));
-    var soapBody = result['soapenv:Body'];
-    var financingRequest = soapBody['ser:financingRequest'];
-    var amount = financingRequest['ser:amount'];
-    var duration = financingRequest['ser:duration'];
-    var rate = financingRequest['ser:rate'];
-    
-    console.log("amount: " + amount);
-    console.log("duration: " + duration);
-    console.log("rate: " + rate);
-
-    var P = amount;
-    var N = duration;
-    var J = rate / 100;
-    J /= 12;
-    var K = 1 / (Math.pow(1 + J, N));
-    var quote = P * (J / (1 - K));
-    var monthlyPaymentAmount = Math.round(quote * 100) / 100;
-
-    res.set('content-type', 'application/xml');
-    res.set('accept', 'application/xml');
-    res.send("<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ser=\"http://services.think.ibm\"><soapenv:Body><ser:financingResult><ser:paymentAmount>" + monthlyPaymentAmount + "</ser:paymentAmount></ser:financingResult></soapenv:Body></soapenv:Envelope>");
+router.route('/calculate')
+  .post(function (req, res) {
+    console.log(req.body);
+  
+    var parseString = require('xml2js').parseString;
+    var parseOptions = {
+      trim: true,
+      explicitArray: false,
+      ignoreAttrs: true,
+      explicitRoot: false
+    };
+  
+    parseString(req.body, parseOptions, function (err, result) {
+      console.log("XML as JSON:\n" + JSON.stringify(result));
+      var soapBody = result['soapenv:Body'];
+      var financingRequest = soapBody['ser:financingRequest'];
+      var amount = financingRequest['ser:amount'];
+      var duration = financingRequest['ser:duration'];
+      var rate = financingRequest['ser:rate'];
+      
+      console.log("amount: " + amount);
+      console.log("duration: " + duration);
+      console.log("rate: " + rate);
+  
+      var P = amount;
+      var N = duration;
+      var J = rate / 100;
+      J /= 12;
+      var K = 1 / (Math.pow(1 + J, N));
+      var quote = P * (J / (1 - K));
+      var monthlyPaymentAmount = Math.round(quote * 100) / 100;
+  
+      res.set('content-type', 'application/xml');
+      res.set('accept', 'application/xml');
+      res.send("<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ser=\"http://services.think.ibm\"><soapenv:Body><ser:financingResult><ser:paymentAmount>" + monthlyPaymentAmount + "</ser:paymentAmount></ser:financingResult></soapenv:Body></soapenv:Envelope>");
+    });
+  
+  })
+  .head(function (req, res) {
+    res.send(200);
   });
-
-});
 
 module.exports = router;
