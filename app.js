d11dc81f5cb07e26727d86a9b83241d8e1880b80
Tim Baker <bakert@us.ibm.com>
2016-08-08 15:49:19 -0500

    adding oauth tester support

--- app.js
+++ app.js
@@ -5,12 +5,22 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// additions for oauthtester
+var jade = require('jade');
+var session = require('express-session');
+var open = require('open');
+
 // define route fns
 var auth = require('./routes/auth');
 var financing = require('./routes/financing');
+var oauthtester = require('./routes/oauthtester');
 var shipping = require('./routes/shipping');
 
 var app = express();
+
+// additions for oauthtester
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -20,9 +30,18 @@ app.use(bodyParser.text({ type: '*/xml' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// additions for oauthtester
+app.use(session({
+  secret: 'ibmApim4me2',
+  resave: false,
+  saveUninitialized: true
+}));
+global.sess;
+
 // set routes
 app.use('/auth', auth);
 app.use('/financing', financing);
+app.use('/oauthtester', oauthtester);
 app.use('/shipping', shipping);
 
 // catch 404 and forward to error handler
